89af50105daae42b6d27fc5f1f03624ce4e7ed22
mikkelgod <xrq510@alumni.ku.dk>
2020-05-30 12:18:12 +0200

    Added values for runtime tests

--- solution/Program.cs
+++ solution/Program.cs
@@ -9,95 +9,102 @@ namespace Implementeringsprojekt {
 
             Stopwatch stopwatch = new Stopwatch();
 
-            int testN = 1000;
-            int testL = 4;
-            
-            int smallStream = 100000;
+            int l = 15;
+            int s = 10000000;
 
-            int fewKeys = 4;
+            int smallL = 8;
+            int bigL = 15;
+            int testSmall = (int)Math.Pow(2,smallL)*5; 
+            int testBig = (int)Math.Pow(2,bigL)*5;
+
             int manyKeys = 12;
             int bigStream = (int)Math.Pow(2,manyKeys)*5;
-            /*
+        
             // Opg. 1c
-            RunTimeTester test1 = new RunTimeTester(testN,testL,HashChaining.MultiplyShift,"Multiply Shift");
-            RunTimeTester test2 = new RunTimeTester(testN,testL,HashChaining.MultiplyModPrime,"Multiply Mod Prime");
+            RunTimeTester test1 = new RunTimeTester(s,l,HashChaining.MultiplyShift,"Multiply Shift");
+            RunTimeTester test2 = new RunTimeTester(s,l,HashChaining.MultiplyModPrime,"Multiply Mod Prime");
             test1.RunTimer();
             test2.RunTimer();
+            stopwatch.Reset();
             // Opg. 3
             // Testing small stream, few different keys, and Multiply Shift
             stopwatch.Start();
-            Sums.createHashTable(smallStream, fewKeys, HashChaining.MultiplyShift);
-            UInt64 squareSum1 = Sums.squareSum();
+            Sums.createHashTable(testSmall, smallL, HashChaining.MultiplyShift);
+            BigInteger squareSum1 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time1 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
 
             // Testing small stream, few different keys, and Multiply Mod Prime
             stopwatch.Start();
-            Sums.createHashTable(smallStream, fewKeys, HashChaining.MultiplyModPrime);
-            UInt64 squareSum2 = Sums.squareSum();
+            Sums.createHashTable(testSmall, smallL, HashChaining.MultiplyModPrime);
+            BigInteger squareSum2 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time2 = stopwatch.Elapsed;
+            stopwatch.Reset();
             
             // Testing small stream, many different keys, and Multiply Shift
             stopwatch.Start();
-            Sums.createHashTable(smallStream, manyKeys, HashChaining.MultiplyShift);
-            UInt64 squareSum3 = Sums.squareSum();
+            Sums.createHashTable(testSmall, bigL, HashChaining.MultiplyShift);
+            BigInteger squareSum3 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time3 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
             // Testing small stream, many different keys, and Multiply Mod prime
             stopwatch.Start();
-            Sums.createHashTable(smallStream, manyKeys, HashChaining.MultiplyModPrime);
-            UInt64 squareSum4 = Sums.squareSum();
+            Sums.createHashTable(testSmall, bigL, HashChaining.MultiplyModPrime);
+            BigInteger squareSum4 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time4 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
             // Testing big stream, few different keys, and Multiply Shift
             stopwatch.Start();
-            Sums.createHashTable(bigStream, fewKeys, HashChaining.MultiplyShift);
-            UInt64 squareSum5 = Sums.squareSum();
+            Sums.createHashTable(testBig, smallL, HashChaining.MultiplyShift);
+            BigInteger squareSum5 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time5 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
             // Testing big stream, few different keys, and Multiply Mod Prime
             stopwatch.Start();
-            Sums.createHashTable(bigStream, fewKeys, HashChaining.MultiplyModPrime);
-            UInt64 squareSum6 = Sums.squareSum();
+            Sums.createHashTable(testBig, smallL, HashChaining.MultiplyModPrime);
+            BigInteger squareSum6 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time6 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
             // Testing big stream, many diferent keys, and Multiply Shift
             stopwatch.Start();
-            Sums.createHashTable(bigStream, manyKeys, HashChaining.MultiplyShift);
-            UInt64 squareSum7 = Sums.squareSum();
+            Sums.createHashTable(testBig, bigL, HashChaining.MultiplyShift);
+            BigInteger squareSum7 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time7 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
             // Testing big stream, many different keys, and Multiply Mod Prime
             stopwatch.Start();
-            Sums.createHashTable(bigStream, manyKeys, HashChaining.MultiplyShift);
-            UInt64 squareSum8 = Sums.squareSum();
+            Sums.createHashTable(testBig, bigL, HashChaining.MultiplyModPrime);
+            BigInteger squareSum8 = Sums.squareSum();
             stopwatch.Stop();
             TimeSpan time8 = stopwatch.Elapsed;
+            stopwatch.Reset();
 
 
-            Console.WriteLine($"Small stream, few keys, multShift. S = {squareSum1}, Time = {time1} ");
-            Console.WriteLine($"Small stream, few keys, multModPrime. S = {squareSum2}, Time = {time2} ");
-            Console.WriteLine($"Small stream, many keys, multShift. S = {squareSum3}, Time = {time3} ");
-            Console.WriteLine($"Small stream, many keys, multModPrime. S = {squareSum4}, Time = {time4} ");            
-            Console.WriteLine($"Big stream, few keys, multShift. S = {squareSum5}, Time = {time5} ");
-            Console.WriteLine($"Big stream, few keys, multModPrime. S = {squareSum6}, Time = {time6} ");            
-            Console.WriteLine($"Big stream, many keys, multShift. S = {squareSum7}, Time = {time7} ");
-            Console.WriteLine($"Big stream, many keys, multModPrime. S = {squareSum8}, Time = {time8} ");
-            */
+            Console.WriteLine($"Multiply Shift, small stream, few keys: S = {squareSum1}, Time = {time1} ");
+            Console.WriteLine($"Multiply Shift, small stream, many keys: S = {squareSum3}, Time = {time3} ");
+            Console.WriteLine($"Multiply Shift, big stream, few keys: S = {squareSum5}, Time = {time5} ");
+            Console.WriteLine($"Multiply Shift, big stream, many keys: S = {squareSum7}, Time = {time7} ");         
+            Console.WriteLine($"Multiply Mod Prime, small stream, few keys: S = {squareSum2}, Time = {time2} ");
+            Console.WriteLine($"Multiply Mod Prime, small stream, many keys: S = {squareSum4}, Time = {time4} ");   
+            Console.WriteLine($"Multiply Mod Prime, big stream, few keys: S = {squareSum6}, Time = {time6} ");            
+            Console.WriteLine($"Multiply Mod Prime, big stream, many keys: S = {squareSum8}, Time = {time8} ");
 
-            stopwatch.Start();
-            Sums.createHashTable(bigStream, manyKeys, HashChaining.MultiplyShift);
-            BigInteger squareSum = Sums.squareSum();
-            stopwatch.Stop();
-            Console.WriteLine($"{squareSum}: {stopwatch.Elapsed}");
-            
+
+
+            /*
             int[] ts = new int[] { 5, 10, 20 };
 
             for (int i = 0; i < ts.Length; i++) {
@@ -139,7 +146,7 @@ namespace Implementeringsprojekt {
                 }
             }
             
-            /*
+            
             IEnumerable<Tuple<ulong, int>> stream = Stream.CreateStream(6, 2);
             foreach (Tuple<ulong, int> tple in stream) {
                 Console.WriteLine(tple);
